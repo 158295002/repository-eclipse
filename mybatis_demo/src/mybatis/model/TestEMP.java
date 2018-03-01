b746ceae7ebbbcf2540b4d5f3fe6c3cc3232741b
dingqin <qin.ding@e-vada.com>
2018-03-01 20:08:51 +0800

    提交

--- mybatis_demo/src/mybatis/model/TestEMP.java
+++ mybatis_demo/src/mybatis/model/TestEMP.java
@@ -1,6 +1,9 @@
 package mybatis.model;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 
@@ -9,7 +12,9 @@ public class TestEMP {
 	public static void main(String[] args) throws IOException {
 		SqlSession session = MyBatisUtil.getSqlSession();
 		// //直接返回封装好的对象
-		User user = session.selectOne("getUserByID", 1);
+//		User user = session.selectOne("getUserByID", 1);
+//		Map<String,String> map = session.selectOne("getUserById", 1);
+		List<Map<String,String>> list = session.selectList("selectAllTableByschemas","test");
 		// //other word
 		session.commit();
 		session.close();
